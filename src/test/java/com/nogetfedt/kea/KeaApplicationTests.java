45c87af955ecb164047d71dd9f4c0560d1cc6ebd
Troels Smidt <Razorsmidt@hotmail.com>
2020-12-07 10:22:59 +0100

    Initial commit

---
+++ src/test/java/com/nogetfedt/kea/KeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nogetfedt.kea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
