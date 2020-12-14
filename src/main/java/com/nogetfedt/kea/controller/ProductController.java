785a0e1a85e8d0cd8fd4d6fc7c658787e48d684e
Erik <erik851c@stud.kea.dk>
2020-12-14 09:11:42 +0100

    Eriks branch faerdiig fr

--- src/main/java/com/nogetfedt/kea/controller/ProductController.java
+++ src/main/java/com/nogetfedt/kea/controller/ProductController.java
@@ -1,9 +1,17 @@
 package com.nogetfedt.kea.controller;
 
+import com.nogetfedt.kea.model.IntComparer;
+import com.nogetfedt.kea.model.Product;
 import com.nogetfedt.kea.repository.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 @Controller
 public class ProductController {
@@ -38,6 +46,17 @@ public class ProductController {
         //Update
 
         //Delete
+            @RequestMapping("/deleteProduct")
+            public String deleteProduct(Model model){return "deleteProduct";}
+
+            @PostMapping("/deleteProduct")
+            public String deleteProductPost(@ModelAttribute IntComparer intComparer){
+            if (intComparer.compare()){
+            repo.deleteById(intComparer.getInt1());}
+            else{return "deleteProductFailed";}
+             return "deleteProduct";}
+            //modtag 2 ints fra model
+            //hvis de matcher, så prøv at slette produktet med den ID
 
 
 }
--- src/main/java/com/nogetfedt/kea/model/IntComparer.java
+++ src/main/java/com/nogetfedt/kea/model/IntComparer.java
@@ -1,4 +1,34 @@
 package com.nogetfedt.kea.model;
+//I'm sure there's a better way to do this
+
 
 public class IntComparer {
+    private int int1;
+    private int int2;
+
+    public boolean compare(){
+        if (int1==int2){return true;}
+        return false;
+    }
+
+    public int getInt1() {
+        return int1;
+    }
+
+    public int getInt2() {
+        return int2;
+    }
+
+    public void setInt1(int int1) {
+        this.int1 = int1;
+    }
+
+    public void setInt2(int int2) {
+        this.int2 = int2;
+    }
+
+    public IntComparer(int int1, int int2){
+        this.int1 = int1;
+        this.int2 = int2;
+    }
 }
