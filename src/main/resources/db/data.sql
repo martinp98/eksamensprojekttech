487276075a3ae30615317c289fb0ba9c7cb2a9c9
Troels Smidt <Razorsmidt@hotmail.com>
2020-12-15 09:34:58 +0100

    Added to nav + added pics

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO Product (product_Id, product_Name, image_Name, description, price, available) VALUES (1, 'Pretty Dress', 'prettydress.jpg', 'Pretty dress with flowers', 500, 1);
-INSERT INTO Product (product_Id, product_Name, image_Name, description, price, available) VALUES (2, 'Pretty Dress', 'prettydress.jpg', 'Pretty dress with Peaches', 600, 1);
-INSERT INTO Product (product_Id, product_Name, image_Name, description, price, available) VALUES (3, 'Pretty Dress', 'prettydress.jpg', 'Pretty dress with flowers', 500, 1);
-INSERT INTO Product (product_Id, product_Name, image_Name, description, price, available) VALUES (4, 'Ugly Dress', 'prettydress.jpg', 'Pretty dress with flowers', 1000, 1);
-INSERT INTO Product (product_Id, product_Name, image_Name, description, price, available) VALUES (5, 'Hideous Dress', 'prettydress.jpg', 'Pretty dress with flowers', 400, 1);
-INSERT INTO Product (product_Id, product_Name, image_Name, description, price, available) VALUES (6, 'Fantastic Dress', 'prettydress.jpg', 'Pretty dress with flowers', 1700, 1);
-INSERT INTO Product (product_Id, product_Name, image_Name, description, price, available) VALUES (7, 'Magic Dress', 'prettydress.jpg', 'Pretty dress with flowers', 100, 1);
+INSERT INTO Product (product_Id, product_Name, image_Name, description, price, available) VALUES (2, 'Pretty Dress', 'greendress.jpg', 'Pretty dress with Peaches', 600, 1);
+INSERT INTO Product (product_Id, product_Name, image_Name, description, price, available) VALUES (3, 'Pretty Dress', 'threeshirts.jpg', 'Pretty dress with flowers', 500, 1);
+INSERT INTO Product (product_Id, product_Name, image_Name, description, price, available) VALUES (4, 'Ugly Dress', 'bluedress.jpg', 'Pretty dress with flowers', 1000, 1);
+INSERT INTO Product (product_Id, product_Name, image_Name, description, price, available) VALUES (5, 'Hideous Dress', 'kawasaki.jpg', 'Pretty dress with flowers', 400, 1);
+INSERT INTO Product (product_Id, product_Name, image_Name, description, price, available) VALUES (6, 'Fantastic Dress', 'tropical.jpg', 'Pretty dress with flowers', 1700, 1);
+INSERT INTO Product (product_Id, product_Name, image_Name, description, price, available) VALUES (7, 'Magic Dress', 'leopard.jpg', 'Pretty dress with flowers', 100, 1);
