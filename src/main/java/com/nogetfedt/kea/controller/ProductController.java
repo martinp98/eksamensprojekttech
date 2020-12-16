a50ae5f51388f6327ea53bfcc9780b9222d289bb
Troels Smidt <Razorsmidt@hotmail.com>
2020-12-16 15:04:51 +0100

    pic upload

--- src/main/java/com/nogetfedt/kea/controller/ProductController.java
+++ src/main/java/com/nogetfedt/kea/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.nogetfedt.kea.controller;
 
 import com.nogetfedt.kea.model.IntComparer;
+import com.nogetfedt.kea.model.Picture;
 import com.nogetfedt.kea.model.Product;
 import com.nogetfedt.kea.repository.NameSorter;
 import com.nogetfedt.kea.repository.PriceSorter;
@@ -8,11 +9,21 @@ import com.nogetfedt.kea.repository.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -32,9 +43,6 @@ public class ProductController {
         return "index";
     }
 
-    //Crud functions used from ProductRepo extends JpaRepository
-
-
     //Create
             @RequestMapping("/addProduct")
             public String addProduct()
@@ -47,8 +55,39 @@ public class ProductController {
             public String newProduct(Product product)
             {
                 repo.save(product);
-                return "index";
+                return "picUpload";
             }
+
+    @RequestMapping("/picUpload")
+    public String picUpload()
+    {
+        return "picUpload";
+    }
+
+    @PostMapping("/save")
+    public String save(@RequestParam("photo")MultipartFile photo, ModelMap model){
+
+        Picture picture = new Picture();
+
+
+        Path path = Paths.get("./src/main/resources/static/img/");
+
+        try {
+            InputStream inputStream = photo.getInputStream();
+            Files.copy(inputStream, path.resolve(photo.getOriginalFilename()),
+                    StandardCopyOption.REPLACE_EXISTING);
+            picture.setPhoto(photo.getOriginalFilename().toLowerCase());
+            //
+
+            model.addAttribute("PICTURE", picture);
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return "addProduct";
+    }
+
 
 
     //Read
