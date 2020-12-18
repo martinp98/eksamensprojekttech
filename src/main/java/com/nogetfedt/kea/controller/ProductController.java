07edddadf9ac5b69429b944a9f4f1063f941ae50
EmilGC <emilgc@live.dk>
2020-12-18 09:02:10 +0100

    Update

--- src/main/java/com/nogetfedt/kea/controller/ProductController.java
+++ src/main/java/com/nogetfedt/kea/controller/ProductController.java
@@ -2,28 +2,22 @@ package com.nogetfedt.kea.controller;
 
 import com.nogetfedt.kea.model.IntComparer;
 import com.nogetfedt.kea.model.Product;
+import com.nogetfedt.kea.model.ProductService;
 import com.nogetfedt.kea.repository.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import javax.validation.Valid;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 
 @Controller
 public class ProductController {
 
+    private ProductService productService;
     @Autowired
     ProductRepo repo;
-
     //Show frontpage
     @RequestMapping("/")
     public String index()
@@ -56,27 +50,13 @@ public class ProductController {
             //ReadById
 
         //Update
-        @GetMapping("/edit/{id}")
-        public String showUpdateForm(@PathVariable("id") int id, Model model) {
-            Product product = ProductRepo.findById(id)
-                    .orElseThrow(() -> new IllegalArgumentException("Invalid product Id:" + id));
 
-            model.addAttribute("product", product);
-            return "update-product";
-        }
-
-        @PostMapping("/update/{id}")
-        public String updateProduct(@PathVariable("id") int id, @Valid Product product,
-                                    BindingResult result, Model model) {
-            if (result.hasErrors()) {
-                product.setProduct_Id(id);
-                return "update-product";
+            @RequestMapping("/update-product/{id}")
+            public String updateProduct(@PathVariable int id, Model model){
+            model.addAttribute("product", productService.get(id));
+            return "/update-product";
             }
 
-            ProductRepo.save(product);
-            model.addAttribute("product", ProductRepo.findAll());
-            return "redirect:/index";
-        }
 
         //Delete
             @RequestMapping("/deleteProduct")
