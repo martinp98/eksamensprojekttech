30d3d0b6af8632b481370a42c482d41bb97c03e0
martinp98 <martin.p.munk@gmail.com>
2021-01-24 21:33:36 +0100

    unit test

---
+++ src/test/java/com/nogetfedt/kea/controller/ProductControllerTest.java
@@ -0,0 +1,15 @@
+package com.nogetfedt.kea.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductControllerTest {
+
+    @Test
+    void viewProduct() {
+
+        assertTrue("/view" == "/vew");
+
+    }
+}
